14282b91240cffc22c78ffa435417769e5fcbf95
singh-aryan-dev <aryandineshsingh1@gmail.com>
2026-04-15 17:55:14 +0530

    Solved Remove duplicates from the array

---
+++ array/removeDuplicate.js
@@ -0,0 +1,30 @@
+let arr = [0,0,1,1,1,2,2,3,3,4]
+
+function removeDuplicate(arr){
+    
+    //edge case
+    if (arr.length === 0) return [];
+
+    let x = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        
+        if (arr[i] != arr[x]) {
+            x = x + 1;
+            arr[x] = arr[i]
+        }
+
+    }
+
+    //returns the number of unique elements
+    // return x + 1;
+
+    // return only unique portion of array
+    return arr.slice(0, x + 1);
+
+    //return array with all the unique element at the start and rest as it is.
+    //return arr
+    
+};
+
+console.log(removeDuplicate(arr))
